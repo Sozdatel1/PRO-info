4f3c954eb754b7323629ee223d34da1c8225285d
Sozdstel1 <chess-pocht@ya.ru>
2026-01-12 21:51:37 +0300

    изменитьфакты

--- scripts/main.js
+++ scripts/main.js
@@ -63,6 +63,62 @@ function checkHoliday() {
 // Запускаем при загрузке страницы
 window.onload = checkHoliday;
 
+const facts = {
+
+"01-12": "Осьминоги имеют три сердца!",
+"01-13": "Осьминоги имеют три сердца!",
+"01-14": "Осьминоги имеют три сердца!",
+"01-15": "Осьминоги имеют три сердца!",
+"01-16": "Осьминоги имеют три сердца!",
+"01-17": "Осьминоги имеют три сердца!",
+"01-18": "Осьминоги имеют три сердца!",
+"01-19": "Осьминоги имеют три сердца!",
+"01-20": "",
+// "01-21":
+// "01-22":
+// "01-23":
+// "01-24":
+// "01-25":
+// "01-26":
+// "01-27":
+// "01-28":
+// "01-29":
+// "01-30":
+// "01-31":
+// "02-01":
+// "02-02":
+// "02-03":
+// "02-04":
+// "02-05":
+// "02-06":
+// "02-07":
+// "02-08":
+// "02-09":
+// "02-10":
+// "02-11":
+// "02-12":
+};
+
+function checkFact() {
+  const today = new Date();
+  const month = today.getMonth() + 1; // Месяцы с 1
+  const day = today.getDate();
+
+  const dateKey = `${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+  const factsName = facts[dateKey];
+  const displayDiv = document.getElementById('fact');
+
+  if (displayDiv) {
+    if (factsName) {
+      displayDiv.innerHTML = `<h2>Факт недели: ${factsName}</h2>`;
+    } else {
+      displayDiv.innerHTML = `<h2>На сегодня факта нет</h2>`;
+    }
+  }
+}
+
+// Запускаем при загрузке страницы
+window.onload = checkFact;
 const snowContainer = document.getElementById('snow');
 
 function createSnowflake() {
@@ -99,19 +155,4 @@ window.addEventListener('load', function() {
     setTimeout(function() {
       document.getElementById('cookieAlert').style.display = 'block';
     }, 3000);
-});
-document.addEventListener('DOMContentLoaded', () => {
-    const factElement = document.getElementById('weekly-fact'); // Элемент, куда вы хотите вставить факт
-    const apiUrl = 'https://www.factroom.ru/eda'; // Замените на реальный адрес API
-
-    fetch(apiUrl)
-        .then(response => response.json()) // Преобразуем ответ в формат JSON
-        .then(data => {
-            // Вставляем полученный факт в ваш HTML-элемент
-            factElement.textContent = data.fact; // Или data.text, зависит от API
-        })
-        .catch(error => {
-            console.error('Ошибка при получении факта:', error);
-            factElement.textContent = 'К сожалению, сегодня фактов нет.';
-        });
 });
