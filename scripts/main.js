e62ee8aca63839cc50820b3a3c2ccee1a06f5358
Sozdatel1 <chess-pocht@ya.ru>
2026-01-26 20:51:21 +0300

    окончательно закончить систему онлайна

--- scripts/main.js
+++ scripts/main.js
@@ -262,7 +262,10 @@ updateVacationCountdown(); // Инициализация сразу
 
 
 // Инициализация Swiper
+const sliderElement = document.querySelector('.my-slider');
+ if (sliderElement) {
 const swiper = new Swiper('.my-slider', {
+   
   loop: true,
   speed: 500,
   autoplay: {
@@ -286,5 +289,31 @@ const swiper = new Swiper('.my-slider', {
       spaceBetween: 0
     }
   }
-    // effect: 'slide',
 });
+}else {
+    console.log("Слайдера на этой странице нет, идем дальше...");
+}
+
+    const RENDER_URL = 'https://pro-info-api.onrender.com';
+    
+    // Подключаемся с настройками, чтобы быстрее работало через прокси
+    const socket = io(RENDER_URL, {
+        transports: ['websocket', 'polling']
+    });
+
+    const counterElement = document.getElementById('online-counter');
+
+    socket.on('updateCount', (count) => {
+        counterElement.innerText = count;
+        console.log('Текущий онлайн:', count);
+    });
+
+    socket.on('connect_error', (error) => {
+        console.log('Ошибка подключения:', error);
+        // counterElement.innerText = 'пробуждаю сервер...';
+        counterElement.innerHTML = '<span class="spinner"></span>';
+    });
+
+    socket.on('connect', () => {
+        console.log('Успешно подключено к серверу!');
+    });
